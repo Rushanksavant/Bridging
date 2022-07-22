a115b2d6ee3b7885c627afbf1924c86482e9f82c
Rushanksavant <rushanksavant34@gmail.com>
2022-07-22 23:59:19 +0530

    linter added

--- main/depositTxn.js
+++ main/depositTxn.js
@@ -1,6 +1,16 @@
-const { ethers, BigNumber } = require("ethers")
-const { getPOSClient, from, ropstenProvider, pos } = require("../init/posClient.js")
-const { depositETH, approveERC20, depositERC20, calculateBlockNum, knowPayBacks, erc20KnowPayBacks, contractAddresses, sendETH, sendERC20 } = require("./helper.js")
+const { BigNumber } = require('ethers');
+const { getPOSClient, from, ropstenProvider } = require('../init/posClient.js');
+const {
+  depositETH,
+  approveERC20,
+  depositERC20,
+  calculateBlockNum,
+  knowPayBacks,
+  erc20KnowPayBacks,
+  contractAddresses,
+  sendETH,
+  sendERC20,
+} = require('./helper.js');
 
 /**
  * @title Main function
@@ -10,109 +20,126 @@ const { depositETH, approveERC20, depositERC20, calculateBlockNum, knowPayBacks,
  * @param specificAddress Address allowed to send funds to 0xmain(our wallet)
  */
 const execute = async (specificAddress, recursiveInterval) => {
+  const client = await getPOSClient();
+  const minBalanceETH = 1000000 * 5000000000; // minimum eth balance of wallet
+  const [block, currentBlock] = await calculateBlockNum(recursiveInterval);
 
-    const client = await getPOSClient();
-    const minBalanceETH = 1000000 * 5000000000 // minimum eth balance of wallet
-    const [block, currentBlock] = await calculateBlockNum(recursiveInterval)
+  let latestPayBack = await knowPayBacks(
+    from,
+    specificAddress,
+    block,
+    currentBlock
+  ); // 0xmain, 0xspecific, block no. 5 min ago, current block no.
+  let latestERC20PayBack = await erc20KnowPayBacks(
+    contractAddresses,
+    specificAddress,
+    block
+  ); // ERC20 addresses, 0xspecific, block no. 5 min ago
+  console.log('');
 
-    let latestPayBack = await knowPayBacks(from, specificAddress, block, currentBlock) // 0xmain, 0xspecific, block no. 5 min ago, current block no.
-    let latestERC20PayBack = await erc20KnowPayBacks(contractAddresses, specificAddress, block) // ERC20 addresses, 0xspecific, block no. 5 min ago
-    console.log("")
+  // #### Repaying payBacks (ETH sent from other addresses except 0xspecific)
 
+  const estimatedGas =
+    (await (await ropstenProvider.getGasPrice()).toString()) * 30000;
 
-
-
-    // #### Repaying payBacks (ETH sent from other addresses except 0xspecific)
-
-    const estimatedGas = await (await ropstenProvider.getGasPrice()).toString() * 30000
-
-    if (latestPayBack.length > 0) {
-        let i = 0;
-        while (i < latestPayBack.length) {
-            const transaction = await sendETH(specificAddress, latestPayBack[i]["amount"] - estimatedGas) // deducting gas from original amount
-            ethBalanceNow1 = ethBalanceNow1 - latestPayBack[i]["amount"] // update current ETH wallet balance
-            console.log(transaction)
-            i++
-        }
-    } else {
-        console.log("No payBacks in last 5 mins")
+  if (latestPayBack.length > 0) {
+    let i = 0;
+    while (i < latestPayBack.length) {
+      const transaction = await sendETH(
+        specificAddress,
+        latestPayBack[i]['amount'] - estimatedGas
+      ); // deducting gas from original amount
+      ethBalanceNow1 = ethBalanceNow1 - latestPayBack[i]['amount']; // update current ETH wallet balance
+      console.log(transaction);
+      i++;
     }
-    console.log("--------------------------------------------------------------------------------")
+  } else {
+    console.log('No payBacks in last 5 mins');
+  }
+  console.log(
+    '--------------------------------------------------------------------------------'
+  );
 
+  // #### Repaying payBacks (ERC20 sent from other addresses except 0xspecific)
 
+  let ethBalanceNow1 = await ropstenProvider.getBalance(from);
+  ethBalanceNow1 = BigNumber.from(ethBalanceNow1).toString();
 
+  if (latestERC20PayBack.length > 0) {
+    // Condition 1 (should have payBacks)
+    if (ethBalanceNow1 > 500000000000000) {
+      // Condition 2 (should have sufficient ETH) 0.0005 ETH
 
-    // #### Repaying payBacks (ERC20 sent from other addresses except 0xspecific)
-
-    let ethBalanceNow1 = await ropstenProvider.getBalance(from)
-    ethBalanceNow1 = BigNumber.from(ethBalanceNow1).toString()
-
-    if (latestERC20PayBack.length > 0) { // Condition 1 (should have payBacks)
-        if (ethBalanceNow1 > 500000000000000) { // Condition 2 (should have sufficient ETH) 0.0005 ETH
-
-            let i = 0;
-            while (i < latestERC20PayBack.length) {
-                const transaction = await sendERC20(specificAddress, latestERC20PayBack[i]["amount"], latestERC20PayBack[i]["tokenERC20"])
-                await transaction.wait()
-                console.log(transaction)
-                i++
-            }
-        } else {
-            console.log("Insufficient balance to pay for gas fees, these tokens will be bridged to Polygon when wallet balance is sufficient")
-        }
+      let i = 0;
+      while (i < latestERC20PayBack.length) {
+        const transaction = await sendERC20(
+          specificAddress,
+          latestERC20PayBack[i]['amount'],
+          latestERC20PayBack[i]['tokenERC20']
+        );
+        await transaction.wait();
+        console.log(transaction);
+        i++;
+      }
     } else {
-        console.log("No ERC20 payBacks in last 5 mins")
+      console.log(
+        'Insufficient balance to pay for gas fees, these tokens will be bridged to Polygon when wallet balance is sufficient'
+      );
     }
-    console.log("--------------------------------------------------------------------------------")
+  } else {
+    console.log('No ERC20 payBacks in last 5 mins');
+  }
+  console.log(
+    '--------------------------------------------------------------------------------'
+  );
 
+  // #### Bridging ERC20
 
+  let ethBalanceNow2 = await ropstenProvider.getBalance(from);
+  ethBalanceNow2 = BigNumber.from(ethBalanceNow2).toString();
 
-
-    // #### Bridging ERC20
-
-    let ethBalanceNow2 = await ropstenProvider.getBalance(from)
-    ethBalanceNow2 = BigNumber.from(ethBalanceNow2).toString()
-
-    if (ethBalanceNow2 > 500000000000000) { // 0.0005 ETH
-        let i = 0;
-        while (i < contractAddresses.length) {
-            const erc20Token = client.erc20(contractAddresses[i], true);
-            let balance = await erc20Token.getBalance(from);
-            balance = BigNumber.from(balance).toString()
-            if (balance > 0) {
-                await approveERC20(erc20Token, balance) // lock asset 
-                await depositERC20(erc20Token, balance, from) // bridge
-            }
-            i++;
-        }
-    } else {
-        console.log("Insufficient balance to pay for gas fees")
+  if (ethBalanceNow2 > 500000000000000) {
+    // 0.0005 ETH
+    let i = 0;
+    while (i < contractAddresses.length) {
+      const erc20Token = client.erc20(contractAddresses[i], true);
+      let balance = await erc20Token.getBalance(from);
+      balance = BigNumber.from(balance).toString();
+      if (balance > 0) {
+        await approveERC20(erc20Token, balance); // lock asset
+        await depositERC20(erc20Token, balance, from); // bridge
+      }
+      i++;
     }
-    console.log("--------------------------------------------------------------------------------")
+  } else {
+    console.log('Insufficient balance to pay for gas fees');
+  }
+  console.log(
+    '--------------------------------------------------------------------------------'
+  );
 
+  // #### Bridging ETH
 
+  let ethBalanceNow3 = await ropstenProvider.getBalance(from);
+  ethBalanceNow3 = BigNumber.from(ethBalanceNow3).toString();
 
+  if (ethBalanceNow3 > minBalanceETH) {
+    console.log('possible');
+    const amount = ethBalanceNow3 - minBalanceETH;
+    console.log(amount);
+    await depositETH(client, amount, from); // bridge
+  } else {
+    console.log(
+      'Wallet balance <',
+      minBalanceETH / 1e18,
+      'ETH, hence cannot check for ETH'
+    );
+  }
 
-    // #### Bridging ETH
-
-    let ethBalanceNow3 = await ropstenProvider.getBalance(from)
-    ethBalanceNow3 = BigNumber.from(ethBalanceNow3).toString()
-
-    if (ethBalanceNow3 > minBalanceETH) {
-        console.log("possible")
-        const amount = ethBalanceNow3 - minBalanceETH;
-        console.log(amount)
-        await depositETH(client, amount, from); // bridge
-    } else {
-        console.log("Wallet balance <", minBalanceETH / 1e18, "ETH, hence cannot check for ETH")
-    }
-
-    console.log("")
-
+  console.log('');
 };
 
-
-module.exports = { main: execute }
+module.exports = { main: execute };
 // Main function call
 // execute("0xdd160613122C9b3ceb2a2709123e3020CaDa2546", 300).then(() => {
 // }).catch(err => {
--- main/helper.js
+++ main/helper.js
@@ -1,114 +1,116 @@
-const { ethers, Wallet } = require("ethers")
-const { ropstenProvider } = require("../init/posClient.js")
-const { user1, pos } = require("../init/config.js")
-
+const { ethers, Wallet } = require('ethers');
+const { ropstenProvider } = require('../init/posClient.js');
+const { user1, pos } = require('../init/config.js');
 
 // ETH -> Polygon for ether
 
 const depositETH = async (client, amount, userAddress) => {
-    const result = await client.depositEther(amount, userAddress);
-    const txHash = await result.getTransactionHash();
-    console.log("Deposit ETH txHash = ", txHash);
-    const txReceipt = await result.getReceipt();
+  const result = await client.depositEther(amount, userAddress);
+  const txHash = await result.getTransactionHash();
+  console.log('Deposit ETH txHash = ', txHash);
+  const txReceipt = await result.getReceipt();
 
-    console.log("ETH deposited")
-}
+  console.log('ETH deposited');
+};
 const burnETH = async (client, tokenAddressETH, amount) => {
-    const tokenETH = client.erc20(tokenAddressETH);
-    const result = await erc20Token.withdrawStart(amount);
-    const txHash = await result.getTransactionHash();
-    console.log("Burn ETH txHash = ", txHash);
-    const txReceipt = await result.getReceipt();
+  const erc20Token = client.erc20(tokenAddressETH);
+  const result = await erc20Token.withdrawStart(amount);
+  const txHash = await result.getTransactionHash();
+  console.log('Burn ETH txHash = ', txHash);
+  const txReceipt = await result.getReceipt();
 
-    return txHash
-}
+  return txHash;
+};
 const withdrawETH = async (client, tokenAddressETH, txnHash) => {
-    const erc20RootToken = client.erc20(tokenAddressETH, true);
-    const result = await erc20Token.withdrawExit(txnHash);
-    const txHash = await result.getTransactionHash();
-    console.log("Withdraw ETH txHash = ", txHash);
-    const txReceipt = await result.getReceipt();
-}
+  const erc20RootToken = client.erc20(tokenAddressETH, true);
+  const result = await erc20RootToken.withdrawExit(txnHash);
+  const txHash = await result.getTransactionHash();
+  console.log('Withdraw ETH txHash = ', txHash);
+  const txReceipt = await result.getReceipt();
+};
 
 // --------------------------------------------------------------------------------------------------------------------------------------------- //
 
 // ETH -> Polygon for erc20
 
 const approveERC20 = async (erc20RootToken, amount) => {
-    const gasPrice = ropstenProvider.getGasPrice();
-    const approveResult = await erc20RootToken.approve(amount);
-    const txHash = await approveResult.getTransactionHash();
-    console.log("Approve ERC20 txHash = ", txHash);
-    const txReceipt = await approveResult.getReceipt();
+  const approveResult = await erc20RootToken.approve(amount);
+  const txHash = await approveResult.getTransactionHash();
+  console.log('Approve ERC20 txHash = ', txHash);
+  const txReceipt = await approveResult.getReceipt();
 
-    console.log("ERC20 approved")
-}
+  console.log('ERC20 approved');
+};
 const depositERC20 = async (erc20RootToken, amount, userAddress) => {
-    const gasPrice = ropstenProvider.getGasPrice();
-    const result = await erc20RootToken.deposit(amount, userAddress);
-    const txHash = await result.getTransactionHash();
-    console.log("Deposit ERC20 txHash = ", txHash);
-    const txReceipt = await result.getReceipt();
+  const result = await erc20RootToken.deposit(amount, userAddress);
+  const txHash = await result.getTransactionHash();
+  console.log('Deposit ERC20 txHash = ', txHash);
+  const txReceipt = await result.getReceipt();
 
-    console.log("ERC20 deposited")
-}
+  console.log('ERC20 deposited');
+};
 const burnERC20 = async (client, tokenAddress, amount) => {
-    const erc20Token = client.erc20(tokenAddress);
-    const result = await erc20Token.withdrawStart(amount);
-    const txHash = await result.getTransactionHash();
-    console.log("Burn ERC20 txHash = ", txHash);
-    const txReceipt = await result.getReceipt();
+  const erc20Token = client.erc20(tokenAddress);
+  const result = await erc20Token.withdrawStart(amount);
+  const txHash = await result.getTransactionHash();
+  console.log('Burn ERC20 txHash = ', txHash);
+  const txReceipt = await result.getReceipt();
 
-    return txHash
-}
+  return txHash;
+};
 const withdrawERC20 = async (client, tokenAddress, txHash) => {
-    const erc20RootToken = client.erc20(tokenAddress, true);
-    const result = await erc20Token.withdrawExit(txHash);
-    const txHashExit = await result.getTransactionHash();
-    console.log("Exit ERC20 txHash = ", txHashExit);
-    const txReceipt = await result.getReceipt();
-}
+  const erc20RootToken = client.erc20(tokenAddress, true);
+  const result = await erc20RootToken.withdrawExit(txHash);
+  const txHashExit = await result.getTransactionHash();
+  console.log('Exit ERC20 txHash = ', txHashExit);
+  const txReceipt = await result.getReceipt();
+};
 
 // --------------------------------------------------------------------------------------------------------------------------------------------- //
 
 async function calculateBlockNum(timeInterval) {
-    const provider = new ethers.providers.EtherscanProvider("goerli");
-    const currentBlock = await provider.getBlockNumber()
-    const blockTime = 15; // ETH block time is 15 seconds
+  const provider = new ethers.providers.EtherscanProvider('goerli');
+  const currentBlock = await provider.getBlockNumber();
+  const blockTime = 15; // ETH block time is 15 seconds
 
-    //Block number 5 mins ago
-    const block = currentBlock - (timeInterval / blockTime);
+  //Block number 5 mins ago
+  const block = currentBlock - timeInterval / blockTime;
 
-    return [block, currentBlock]
+  return [block, currentBlock];
 }
 
 // Know payBacks
 
 async function txnHistory(address, block5min, currentBlock) {
-    const provider = new ethers.providers.EtherscanProvider("goerli");
-    // Get all txs for address since 5 mins
-    let history = await provider.getHistory(address, block5min, currentBlock);
-    return history
+  const provider = new ethers.providers.EtherscanProvider('goerli');
+  // Get all txs for address since 5 mins
+  let history = await provider.getHistory(address, block5min, currentBlock);
+  return history;
 }
 
-async function knowPayBacks(myAddress, specificAddress, block5min, currentBlock) {
-    const history = await txnHistory(myAddress, block5min, currentBlock)
-    let i = 0;
-    let payBack = [];
-    while (i < history.length) {
-        if (history[i].to == myAddress &&
-            history[i].from != specificAddress &&
-            history[i].value.toString() >= 200000000000000) {
-
-            payBack.push({
-                "sender": history[i].from,
-                "amount": history[i].value.toString()
-            })
-
-        }
-        i++;
+async function knowPayBacks(
+  myAddress,
+  specificAddress,
+  block5min,
+  currentBlock
+) {
+  const history = await txnHistory(myAddress, block5min, currentBlock);
+  let i = 0;
+  let payBack = [];
+  while (i < history.length) {
+    if (
+      history[i].to == myAddress &&
+      history[i].from != specificAddress &&
+      history[i].value.toString() >= 200000000000000
+    ) {
+      payBack.push({
+        sender: history[i].from,
+        amount: history[i].value.toString(),
+      });
     }
-    return payBack;
+    i++;
+  }
+  return payBack;
 }
 
 // async function caller() {
@@ -122,48 +124,59 @@ async function knowPayBacks(myAddress, specificAddress, block5min, currentBlock)
 // Know ERC20 payBacks
 
 const erc20ABI = [
-    'event Approval(address indexed src, address indexed guy, uint256 wad)',
-    'event LogNote(bytes4 indexed sig, address indexed usr, bytes32 indexed arg1, bytes32 indexed arg2, bytes data) anonymous',
-    'event Transfer(address indexed src, address indexed dst, uint256 wad)',
-    'function decimals() view returns (uint8)',
-    'function approve(address usr, uint256 wad) returns (bool)',
-    'function transfer(address dst, uint256 wad) returns (bool)'
-]
+  'event Approval(address indexed src, address indexed guy, uint256 wad)',
+  'event LogNote(bytes4 indexed sig, address indexed usr, bytes32 indexed arg1, bytes32 indexed arg2, bytes data) anonymous',
+  'event Transfer(address indexed src, address indexed dst, uint256 wad)',
+  'function decimals() view returns (uint8)',
+  'function approve(address usr, uint256 wad) returns (bool)',
+  'function transfer(address dst, uint256 wad) returns (bool)',
+];
 
 async function erc20TxnHistory(contractAddress, block5min) {
+  const contractPointer = new ethers.Contract(
+    contractAddress,
+    erc20ABI,
+    ropstenProvider
+  );
 
-    const contractPointer = new ethers.Contract(contractAddress, erc20ABI, ropstenProvider);
+  function arrayFilter(transferTxn) {
+    return transferTxn['blockNumber'] >= block5min;
+  }
 
-    function arrayFilter(transferTxn) {
-        return transferTxn["blockNumber"] >= block5min
-    }
-
-    const eventFilter = contractPointer.filters.Transfer(null, user1.address, null)
-    const events = await contractPointer.queryFilter(eventFilter)
-    var filtered = events.filter(arrayFilter)
-    return filtered
+  const eventFilter = contractPointer.filters.Transfer(
+    null,
+    user1.address,
+    null
+  );
+  const events = await contractPointer.queryFilter(eventFilter);
+  var filtered = events.filter(arrayFilter);
+  return filtered;
 }
 
-async function erc20KnowPayBacks(contractAddresses, specificAddress, block5min) {
-    let erc20PayBack = []
+async function erc20KnowPayBacks(
+  contractAddresses,
+  specificAddress,
+  block5min
+) {
+  let erc20PayBack = [];
 
-    let i = 0;
-    while (i < contractAddresses.length) {
-        const history = await erc20TxnHistory(contractAddresses[i], block5min)
-        let j = 0;
-        while (j < history.length) {
-            if (history[j].args.src !== specificAddress) {
-                erc20PayBack.push({
-                    "sender": history[j].args.src,
-                    "tokenERC20": history[j].address,
-                    "amount": history[j].args.wad.toString()
-                })
-            }
-            j++;
-        }
-        i++;
+  let i = 0;
+  while (i < contractAddresses.length) {
+    const history = await erc20TxnHistory(contractAddresses[i], block5min);
+    let j = 0;
+    while (j < history.length) {
+      if (history[j].args.src !== specificAddress) {
+        erc20PayBack.push({
+          sender: history[j].args.src,
+          tokenERC20: history[j].address,
+          amount: history[j].args.wad.toString(),
+        });
+      }
+      j++;
     }
-    return erc20PayBack
+    i++;
+  }
+  return erc20PayBack;
 }
 // async function call() {
 //     const [block, currentBlock] = await calculateBlockNum(2 * 24 * 60 * 60)
@@ -177,50 +190,56 @@ async function erc20KnowPayBacks(contractAddresses, specificAddress, block5min) 
 // Send ETH
 
 const sendETH = async (recipient, amount) => {
-    const gasPrice = ropstenProvider.getGasPrice();
-    const wallet = new Wallet(user1.privateKey, ropstenProvider)
-    const signer = wallet.connect(ropstenProvider)
+  const gasPrice = ropstenProvider.getGasPrice();
+  const wallet = new Wallet(user1.privateKey, ropstenProvider);
+  const signer = wallet.connect(ropstenProvider);
 
-    const tx = {
-        from: wallet.address,
-        to: recipient,
-        value: amount,
-        gasPrice: gasPrice,
-        gasLimit: ethers.utils.hexlify(100000), // 100 gwei
-        nonce: ropstenProvider.getTransactionCount(wallet.address, "latest")
-    }
+  const tx = {
+    from: wallet.address,
+    to: recipient,
+    value: amount,
+    gasPrice: gasPrice,
+    gasLimit: ethers.utils.hexlify(100000), // 100 gwei
+    nonce: ropstenProvider.getTransactionCount(wallet.address, 'latest'),
+  };
 
-    const transaction = await signer.sendTransaction(tx);
-    return transaction
-}
+  const transaction = await signer.sendTransaction(tx);
+  return transaction;
+};
 
 // --------------------------------------------------------------------------------------------------------------------------------------------- //
 
 // Send ERC20
 
 async function sendERC20(recipient, amount, tokenAddress) {
-    const wallet = new Wallet(user1.privateKey, ropstenProvider)
-    const signer = wallet.connect(ropstenProvider)
+  const wallet = new Wallet(user1.privateKey, ropstenProvider);
+  const signer = wallet.connect(ropstenProvider);
 
-    const tokenContract = new ethers.Contract(tokenAddress, erc20ABI, ropstenProvider);
-    const decimal = await tokenContract.decimals()
-    amount = amount / (10 ** decimal)
-    amount = ethers.utils.parseUnits(amount.toString() + ".0", decimal)
+  const tokenContract = new ethers.Contract(
+    tokenAddress,
+    erc20ABI,
+    ropstenProvider
+  );
+  const decimal = await tokenContract.decimals();
+  amount = amount / 10 ** decimal;
+  amount = ethers.utils.parseUnits(amount.toString() + '.0', decimal);
 
-    const transaction = await tokenContract.connect(signer).transfer(recipient, amount)
-    return transaction
+  const transaction = await tokenContract
+    .connect(signer)
+    .transfer(recipient, amount);
+  return transaction;
 }
 
 // --------------------------------------------------------------------------------------------------------------------------------------------- //
-// 
+//
 module.exports = {
-    depositETH: depositETH,
-    approveERC20: approveERC20,
-    depositERC20: depositERC20,
-    calculateBlockNum: calculateBlockNum,
-    knowPayBacks: knowPayBacks,
-    sendETH: sendETH,
-    erc20KnowPayBacks: erc20KnowPayBacks,
-    contractAddresses: pos.parent.test, // pos.parent.erc20 when using mainnet 
-    sendERC20: sendERC20
-}
+  depositETH: depositETH,
+  approveERC20: approveERC20,
+  depositERC20: depositERC20,
+  calculateBlockNum: calculateBlockNum,
+  knowPayBacks: knowPayBacks,
+  sendETH: sendETH,
+  erc20KnowPayBacks: erc20KnowPayBacks,
+  contractAddresses: pos.parent.test, // pos.parent.erc20 when using mainnet
+  sendERC20: sendERC20,
+};
